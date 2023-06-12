926d7c9676adebd8cdcbd994cf1ec951c96206d8
kcho760 <kcho760@gmail.com>
2023-06-12 09:34:36 -0400

    frontend-setup complete

---
+++ frontend/stores/store.js
@@ -0,0 +1,37 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import session from './session';
+const rootReducer = combineReducers({
+  session
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require('redux-logger').default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+const nullErrors = null;
+
+export const sessionErrorsReducer = (state = nullErrors, action) => {
+  switch(action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+    case CLEAR_SESSION_ERRORS:
+      return nullErrors;
+    default:
+      return state;
+  }
+};
+
+export default configureStore;
