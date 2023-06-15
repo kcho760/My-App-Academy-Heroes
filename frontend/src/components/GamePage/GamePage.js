dcbcee1ba465b9bde45a2d63eb4ba490132615c5
kcho760 <kcho760@gmail.com>
2023-06-15 03:03:19 -0400

    enemy-screen-transition

--- frontend/src/components/GamePage/GamePage.js
+++ frontend/src/components/GamePage/GamePage.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchQuestions } from "../../store/questions";
 import "./GamePage.css";
 import PlayerStat from "../PlayerStat/PlayerStat";
 import Question from "../Question/Question";
 import LoadingPage from "../Utils/Loading/LoadingPage";
-import { Redirect } from "react-router-dom/";
+import { Redirect } from "react-router-dom";
 import Enemy from "../Enemy/enemy.js";
 import enemy1 from "../Enemy/enemy1.js";
 import enemy2 from "../Enemy/enemy2.js";
@@ -24,25 +24,29 @@ const GamePage = () => {
   const [gameOver, setGameover] = useState(false);
   const [message, setMessage] = useState(<></>);
   const [enemy, setEnemy] = useState(enemy1);
-  const [round, setRound] = useState(1); // for use when kill enemy
-  const [playerAttack, setPlayerAttack] = useState(50); // for use when kill enemy
+  const [round, setRound] = useState(1);
+  const [playerAttack, setPlayerAttack] = useState(50);
   const [showExplosion, setShowExplosion] = useState(false);
   const [showPlayerExplosion, setShowPlayerExplosion] = useState(false);
-  const [attackAnimation, setAttackAnimation] = useState(false); // for use when kill enemy
+  const [attackAnimation, setAttackAnimation] = useState(false);
+  const [isDefeated, setIsDefeated] = useState(false);
+  const [shouldAnimateOut, setShouldAnimateOut] = useState(false);
+
   useEffect(() => {
     dispatch(fetchQuestions());
   }, []);
-  
+
   useEffect(() => {
     dispatch(updateUser(user));
   }, [user.gold, user.health]);
+
   if (!user) return <Redirect to="/login" />;
   if (questions.length === 0) return <LoadingPage />;
 
   const max = questions.length;
   const question = questions[idx];
   const correctAnswer = question.answers[question.correct_answer];
-  console.log(correctAnswer)
+  console.log(correctAnswer);
 
   const handleSelect = (e) => {
     e.preventDefault();
@@ -50,37 +54,33 @@ const GamePage = () => {
     const newTotal = totalAnswered + 1;
     setTotalAnswered(newTotal);
     if (e.target.value === correctAnswer) {
-      // stuff that happens when user answers correctly
+      // Correct answer logic
       setAttackAnimation(true);
       setTimeout(() => {
         setShowExplosion(true);
       }, 1000);
-        
+
       setTimeout(() => {
-          setAttackAnimation(false);
-          setShowExplosion(false);
-        }, 2000);
+        setAttackAnimation(false);
+        setShowExplosion(false);
+      }, 2000);
 
       handleEnemyLogic();
       const newCorrects = totalCorrects + 1;
       setTotalCorrects(newCorrects);
       user.gold += 1;
       setMessage(
-        <div className="answer-result correct">Nice. That was correct !</div>
+        <div className="answer-result correct">Nice. That was correct!</div>
       );
-      // deduct enemy hp and check if enemy hp will be 0 or lower
-      // count round
     } else {
-      // stuff that happends when user answers wrong
-
+      // Wrong answer logic
       setShowPlayerExplosion(true);
 
       setTimeout(() => {
         setShowPlayerExplosion(false);
-
       }, 1000);
 
-      const healthText = document.querySelector(".health-text")
+      const healthText = document.querySelector(".health-text");
       healthText.classList.add("flash-red");
       setTimeout(() => {
         healthText.classList.remove("flash-red");
@@ -95,17 +95,11 @@ const GamePage = () => {
         user.health = 0;
         setGameover(true);
         user.health = 100;
-        // can reset player hp to 100 instead
       } else {
         user.health -= enemy.attack;
       }
     }
 
-
-    // need to dispatch an update to player becuase of gold and hp update
-    //all changes above are temporary and only in frontend store state, reset upon refresh currently
-
-    // giving 2s buffer before switching to next question
     setTimeout(() => {
       if (idx + 1 < max) {
         setIdx((prevIdx) => prevIdx + 1);
@@ -117,53 +111,77 @@ const GamePage = () => {
     }, 2000);
   };
 
-
   const handleEnemyLogic = () => {
-  
     if (enemy.health - playerAttack <= 0) {
       // Handle enemy defeated logic, such as updating round, gaining rewards, etc.
       // For example:
       user.gold += enemy.gold;
-      const newRound = round + 1
+      const newRound = round + 1;
       setRound(newRound);
+  
       if (newRound % 3 === 0) {
-          setEnemy(enemy2);
-          // enemy.health = enemy2.defaultHealth; 
+        setAttackAnimation(true);
+        setTimeout(() => {
+          setEnemy((prevEnemy) => ({
+            ...prevEnemy,
+            health: 0, // Show enemy health as 0 during the delay
+          }));
+          setTimeout(() => {
+            setShouldAnimateOut(true);
+            setTimeout(() => {
+              setEnemy(enemy2);
+              setEnemy((prevEnemy) => ({
+                ...prevEnemy,
+                health: prevEnemy.defaultHealth,
+              }));
+              setShouldAnimateOut(false);
+              setAttackAnimation(false);
+            }, 500);
+          }, 700);
+        }, 1000);
+      } else {
+        setAttackAnimation(true);
+        setTimeout(() => {
+          setEnemy((prevEnemy) => ({
+            ...prevEnemy,
+            health: 0, // Show enemy health as 0 during the delay
+          }));
+          setTimeout(() => {
+            setEnemy(enemy1);
+            setEnemy((prevEnemy) => ({
+              ...prevEnemy,
+              health: prevEnemy.defaultHealth,
+            }));
+            setAttackAnimation(false);
+          }, 700);
+        }, 1000);
       }
-      else {
-        setEnemy(enemy1);
-        // enemy.health = enemy1.defaultHealth;
-      }
-
-      setEnemy((prevEnemy) => ({
-        ...prevEnemy,
-        health: prevEnemy.defaultHealth,
-      }));
-    }else{
+      
+    } else {
       setEnemy((prevEnemy) => ({
         ...prevEnemy,
         health: prevEnemy.health - playerAttack,
       }));
     }
   };
-
+  
   return (
     <div className="game-page-container">
       <div className="game-page-content left">
         <div className="game-content game-user-info">
           <PlayerStat />
         </div>
-          <div className="answered-stats">
-            <div className="total-answered">
-              Total questions anwered: {totalAnswered}
-            </div>
-            <div className="total-correct-answered">
-              Total correct answered: {totalCorrects}
-            </div>
-            <div>
-              <div className="round">Round: {round}</div>
-            </div>
+        <div className="answered-stats">
+          <div className="total-answered">
+            Total questions answered: {totalAnswered}
           </div>
+          <div className="total-correct-answered">
+            Total correct answered: {totalCorrects}
+          </div>
+          <div>
+            <div className="round">Round: {round}</div>
+          </div>
+        </div>
         <div className="game-content game-questions">
           {gameOver ? (
             <div className="">GAME OVER...</div>
@@ -180,11 +198,23 @@ const GamePage = () => {
         </div>
       </div>
       <div className="game-page-content right">
-        <div className="game-content enemy-board">
-          <Enemy enemy={enemy} showExplosion={showExplosion} />
+        <div
+          className={`game-content enemy-board ${
+            shouldAnimateOut ? "animate-out" : ""
+          }`}
+        >
+          <Enemy
+            enemy={enemy}
+            showExplosion={showExplosion}
+            isDefeated={isDefeated}
+          />
         </div>
         <div className="game-content player-board">
-          <GamePlayer user={user} attackAnimation={attackAnimation} showPlayerExplosion={showPlayerExplosion}/>
+          <GamePlayer
+            user={user}
+            attackAnimation={attackAnimation}
+            showPlayerExplosion={showPlayerExplosion}
+          />
         </div>
       </div>
     </div>
