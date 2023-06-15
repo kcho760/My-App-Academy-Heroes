e9b69bd4eb1d1a0f8a9b849486c4b47712578ec4
herilynn <4henrylin17@gmail.com>
2023-06-15 18:13:11 -0400

    Change card bg

--- frontend/src/components/Instructions/Instructions.js
+++ frontend/src/components/Instructions/Instructions.js
@@ -12,7 +12,7 @@ function Instructions() {
   };
 
   return (
-    <div className="modal-container">
+    <div className="instructions-container">
       <div className="modal-tabs">
         
         <button className={`tab ${activeTab === "generalTab" && "active"}`}
@@ -37,23 +37,26 @@ function Instructions() {
             <h1 className= "generalHeader">**"Profile" and "Game" tabs only visible when logged in**</h1>
               <ul className="tabDescription">
                 <li className="li-space">Collect cohortmates for different effects that will help you progress in the game.</li>
-                Not all cohortmates are created equal though. Higher rarity have stronger effects but are less likely to be obtained. To obtain them; pray to the RNG 
-                gods when using "Pull a Card" on your "profile" tab which would give a new card for 10 gold.
+                <li className="li-space">Not all cohortmates are created equal though. Higher rarity have stronger effects </li> 
+                <li className="li-space">but are less likely to be obtained. To obtain them; pray to the RNG gods when using </li>
+                <li className="li-space"> "Pull a Card" on your "profile" tab which would give a new card for 10 gold.</li>
               </ul>
           </div>
         )}
         {activeTab === "gameTab" && (
           <div className="tab">
-              <ul className="tabDescription">On the "game" tab; you'd see waves of enemies on the right side of the screen. 
+              <ul className="tabDescription">
+              <li className="li-space">On the "game" tab; you'd see waves of enemies on the right side of the screen. </li>
               
               <li className="li-space">You deal damage by answering questions on the bottom left of the screen. </li>
               
-              <li className="li-space">For each correct question; you obtain a bit of gold. If you defeat a wave; you receive more gold. </li>
+              <li className="li-space">For each correct question; you obtain a bit of gold. If you defeat a wave; you receive more </li>
               
-              <li className="li-space">After completing a weeks-worth of enemies; you will encounter a TA who will test your knowledge. They're just like the practice waves before but with 
-              some curveballs/caveats that you won't know until the day of but if you ran through the earlier waves at least 10x in half the recommended time; you're good unless your database breaks. </li>
-                
-              <li className="li-space">If you make it past the TA; you obtain even more gold to spend on your gambling addiction in the shop. </li>
+              <li className="li-space">gold. After completing a weeks-worth of enemies; you will encounter a TA who will test your </li>
+              <li className="li-space">knowledge. They're just like the practice waves before but with some curveballs/caveats that </li>
+              <li className="li-space">you won't know until the day of but if you ran through the earlier waves at least 10x in half </li>
+              <li className="li-space">the recommended time; you're good unless your database breaks. If you make it past the TA; you </li>
+              <li className="li-space">obtain even more gold to spend on your gambling addiction in the shop. </li>
               </ul>
           </div>
         )}
